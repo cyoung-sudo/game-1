7954d6660911f14d058a9bb8ee52efcb46c55201
Christopher Young <christopheryoung@MacBook123.local>
2024-04-10 14:39:56 -0700

    added movement layout

--- src/components/controls/Movement.js
+++ src/components/controls/Movement.js
@@ -1,9 +1,35 @@
 import "./Movement.scss"
+// Icons
+import { GoArrowUp } from "react-icons/go";
+import { GoArrowDown } from "react-icons/go";
+import { GoArrowLeft } from "react-icons/go";
+import { GoArrowRight } from "react-icons/go";
+import { GoArrowUpLeft } from "react-icons/go";
+import { GoArrowUpRight } from "react-icons/go";
+import { GoArrowDownLeft } from "react-icons/go";
+import { GoArrowDownRight } from "react-icons/go";
+import { IoIosPlay } from "react-icons/io";
 
 const Movement = () => {
   return (
     <div id="movement">
+      <div>
+        <button><GoArrowUpLeft/></button>
+        <button><GoArrowUp/></button>
+        <button><GoArrowUpRight/></button>
+      </div>
 
+      <div>
+        <button><GoArrowLeft/></button>
+        <button><IoIosPlay/></button>
+        <button><GoArrowRight/></button>
+      </div>
+
+      <div>
+        <button><GoArrowDownLeft/></button>
+        <button><GoArrowDown/></button>
+        <button><GoArrowDownRight/></button>
+      </div>
     </div>
   );
 };
